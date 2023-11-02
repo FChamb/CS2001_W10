7b0e4fd2345762ed715429c7171d67925cea53ac
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-11-02 14:56:41 +0000

    Began implementing mergesort

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,2 +1,16 @@
 public class MergeSort {
+    private int[] array;
+    public MergeSort(int[] array) {
+        this.array = array;
+    }
+
+    public int[] getArray() {
+        return this.array;
+    }
+
+    public void sortArray(int[] array, int left, int right) {
+        if (left < right) {
+            int middle = left + (right - 1) / 2;
+        }
+    }
 }
